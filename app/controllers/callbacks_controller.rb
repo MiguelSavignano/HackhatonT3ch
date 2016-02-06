7bdecec15c63c28b4b9b68139f85eaa2a492ef4c
JavierOrtiz <javierortizconejo@gmail.com>
2016-02-06 14:06:15 +0100

    configurando login social

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,7 +1,6 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        binding.pry
         sign_in_and_redirect @user
     end
     def twitter
