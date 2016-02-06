6c14c85fe38cfd9cdf98f3989d29c972a8894ed5
Sergio Pérez <sergioperez794@gmail.com>
2016-02-06 13:29:51 +0100

    commit inutil

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206131643) do
+ActiveRecord::Schema.define(version: 20160206113352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,8 +88,6 @@ ActiveRecord::Schema.define(version: 20160206131643) do
     t.datetime "updated_at",  null: false
     t.boolean  "verified"
     t.integer  "user_id"
-    t.string   "latitude"
-    t.string   "longitude"
   end
 
   add_index "notices", ["city_id"], name: "index_notices_on_city_id", using: :btree
@@ -126,6 +124,13 @@ ActiveRecord::Schema.define(version: 20160206131643) do
   add_index "users", ["city_id"], name: "index_users_on_city_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "votes", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "notice_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   add_foreign_key "comments", "notices"
   add_foreign_key "comments", "users"
