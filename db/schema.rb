c35153321ed86e0a636e00a0d431817f945010e0
JavierOrtiz <javierortizconejo@gmail.com>
2016-02-06 11:54:01 +0100

    Añadido rol a usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206102630) do
+ActiveRecord::Schema.define(version: 20160206105351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 20160206102630) do
     t.integer  "sash_id"
     t.integer  "level",                  default: 0
     t.string   "locate"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
