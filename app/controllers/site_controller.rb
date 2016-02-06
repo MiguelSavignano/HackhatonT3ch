a8694eb489d0c18e951ae1ffda54866cc8f582c5
JavierOrtiz <javierortizconejo@gmail.com>
2016-02-06 12:28:33 +0100

    Añadiendo registro

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,10 @@
 class SiteController < ApplicationController
   before_action :authenticate_user!
   def index
+    if params[:locate].nil?
+      @notices = Notice.where(locate: params[:locate])
+    else
+      @notices = Notice.where(locate: current_user.locate)
+    end
   end
 end
