5231bd1acf9dbd624601f42486ef21c2ca39102b
JavierOrtiz <javierortizconejo@gmail.com>
2016-02-06 12:44:46 +0100

    Eliminado scaffold y optimizando vista de home

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,6 @@
 class SiteController < ApplicationController
   before_action :authenticate_user!
   def index
-    if params[:locate].nil?
-      @notices = Notice.where(locate: params[:locate])
-    else
-      @notices = Notice.where(locate: current_user.locate)
-    end
+    @notices = Notice.where(city: current_user.city)
   end
 end
